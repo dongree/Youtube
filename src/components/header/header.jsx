7d8c62f48b80905eb1a648d4f79dcc54b606e5b0
dongree <dongwoo0307@naver.com>
2023-03-27 22:48:33 +0900

    set global css and update header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,16 +3,20 @@ import { AiOutlineSearch, AiFillYoutube } from 'react-icons/ai';
 
 export default function Header() {
   return (
-    <header className="flex justify-center items-center">
+    <header className="flex justify-between items-center py-3 px-1 border-b-2 border-gray-400	">
       <div className="flex items-center">
-        <div>
+        <button className="text-4xl text-red-600 mr-1">
           <AiFillYoutube />
-        </div>
-        <div className="text-2xl font-medium">Youtube</div>
+        </button>
+        <div className="text-3xl font-medium text-white">Youtube</div>
       </div>
-      <form className="flex items-center">
-        <input className="w-200" type="text" placeholder="Search..."></input>
-        <button>
+      <form className="flex items-center w-3/5 mr-24">
+        <input
+          className="w-full bg-black text-white h-10 px-3"
+          type="text"
+          placeholder="Search..."
+        ></input>
+        <button className="text-2xl bg-gray-400 h-10 w-10 flex justify-center items-center text-white ">
           <AiOutlineSearch />
         </button>
       </form>
