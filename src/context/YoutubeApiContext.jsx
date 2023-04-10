132e2bfad06de0cbd7ebe3194ad27abb624148a1
dongree <dongwoo0307@naver.com>
2023-04-10 16:17:56 +0900

    refactor youtube, fakeyoutube

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,11 +1,13 @@
 import { createContext, useContext } from 'react';
 import Youtube from '../api/youtube';
-import FakeYoutube from '../api/fakeYoutube';
+import YoutubeClient from '../api/youtubeClient';
+import FakeYoutubeClient from '../api/fakeYoutubeClient';
 
 export const YoutubeApiContext = createContext();
 
-// const youtube = new FakeYoutube();
-const youtube = new Youtube();
+const client = new YoutubeClient();
+// const client = new FakeYoutubeClient()
+const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
   return (
